18c45554df9ed50d1b096a12c17765c08870398d
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-05-14 10:15:21 +0300

    complete all tasks

---
+++ src/js/03-promises.js
@@ -0,0 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormsubmit);
+
+function onFormsubmit(event) {
+  event.preventDefault();
+
+  let delay = +event.target.delay.value;
+  let step = +event.target.step.value;
+  let amount = +event.target.amount.value;
+
+  if ([delay, step, amount].find(value => value < 0)) {
+    Notify.failure('Please, enter correct data > 0');
+    return;
+  }
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
+function createPromise(position, delay) {
+  return new Promise((response, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        response({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
